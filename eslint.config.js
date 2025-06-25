025e4d3be2d6ef1b37f77bd6ef21a9b630ad9abe
Lauri Gates <lauri.gates@forumvirium.fi>
2025-06-25 10:04:07 +0300

    chore: update eslint config
    Migrated to the new eslint config spec

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import js from "@eslint/js";
+import tsEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+  // Global ignores
+  {
+    ignores: ["node_modules/**", "dist/**", "coverage/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        // FoundryVTT globals
+        CONFIG: "readonly",
+        game: "readonly",
+        canvas: "readonly",
+        ui: "readonly",
+        foundry: "readonly",
+        Hooks: "readonly",
+        $: "readonly",
+        randomID: "readonly",
+        debounce: "readonly",
+        // FoundryVTT types
+        AVClient: "readonly",
+        AVMaster: "readonly",
+        AVSettings: "readonly",
+        CameraViews: "readonly",
+        User: "readonly",
+        AudioHelper: "readonly",
+        Dialog: "readonly",
+        Canvas: "readonly",
+        Game: "readonly",
+        // Browser types that might not be available
+        JQuery: "readonly",
+        EventListener: "readonly",
+        MediaDeviceKind: "readonly",
+        MediaStream: "readonly",
+        HTMLVideoElement: "readonly",
+        HTMLElement: "readonly",
+        CustomEvent: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsEslint,
+    },
+    rules: {
+      ...tsEslint.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
